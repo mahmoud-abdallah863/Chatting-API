fd99c19fd3b00073c1dd8ae07ce8e9223d99563e
mahmoud-abdallah863 <mabdallah863@gmail.com>
2020-05-16 06:09:54 +0300

    Added all files

---
+++ Events.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Join: "Join",
+    UserJoinTheChat: 'UserJoinTheChat',
+    MessageDetection: 'MessageDetection',
+    Message: "message",
+    Disconnected: 'disconnect',
+    UserDisconnected: 'Disconnected'
+}
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express'),
+http = require('http'),
+app = express(),
+server = http.createServer(app),
+io = require('socket.io').listen(server);
+
+const { Join, UserJoinTheChat, MessageDetection, Message,
+    Disconnected, UserDisconnected} = require('./Events')
+
+app.get('/', (req, res) => {
+    res.send('char server is running on port 3000')
+})
+
+server.listen(3000, () => {
+    console.log('Node app is running on port 3000')
+})
+
+
+io.on('connection', (socket) => {
+    console.log("user connected")
+
+    socket.on(Join, (nickName) => {
+        console.log(nickName + " : has joined the chat")
+        socket.broadcast.emit(UserJoinTheChat, nickName + " : has joined the chat")
+    })
+
+    socket.on(MessageDetection, (nickName, messageContent) => {
+        console.log(nickName + " : " + messageContent)
+
+        let m = { 'message': messageContent, 'nickName': nickName}
+
+        io.emit(Message, m)
+    })
+
+    socket.on(Disconnected, () => {
+        console.log('user has left')
+
+        socket.broadcast.emit(UserDisconnected, 'user has left')
+    })
+})
